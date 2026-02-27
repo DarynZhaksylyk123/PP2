bf6fe2a86ed798e0d692408975e5f69b026455ca
DarynZhaksylyk <da_zhaksylyk@kbtu.kz>
2026-02-27 22:04:12 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

--- PP2-Tasks/Practice4/generators.py
+++ PP2-Tasks/Practice4/generators.py
@@ -2,8 +2,8 @@
 def square_generator(n):
     for i in range(n + 1):
         yield i * i
-
-for value in square_generator(5):
+n = int(input())
+for value in square_generator(n):
     print(value)
 
 
