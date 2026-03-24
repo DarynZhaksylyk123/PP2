c7f35b58921a9d8446744e56826d309e4402ab7f
DarynZhaksylyk <da_zhaksylyk@kbtu.kz>
2026-03-24 17:55:59 +0500

    Add Practice7 - PhoneBook with PostgreSQL

---
+++ PP2-Tasks/Practice7/phonebook/phonebook.py
@@ -0,0 +1,157 @@
+import psycopg2
+import csv
+from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
+
+# ----Connection----
+
+conn = psycopg2.connect(
+    host=DB_HOST,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
+)
+
+# ----Create Table----
+
+def table():
+    sql = """CREATE TABLE IF NOT EXISTS contacts(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        phone VARCHAR(50) NOT NULL UNIQUE);
+        """
+    with conn.cursor() as cur:
+        cur.execute(sql)
+        conn.commit()
+    
+# ----INSERT----
+
+def insert(name, phone):
+    sql = "INSERT INTO contacts(name, phone) VALUES(%s, %s)"
+    with conn.cursor() as cur:
+        cur.execute(sql, (name, phone))
+        conn.commit()
+
+def insert_console():
+    name = input("Enter name: ")
+    phone = input("Enter phone: ")
+    insert(name, phone)
+    print(f"Added: {name} - {phone}")
+    
+def insert_csv(csv_file):
+    sql = "INSERT INTO contacts(name, phone) VALUES(%s, %s)"
+    with conn.cursor() as cur:
+        with open(csv_file, "r") as f:
+            read = csv.reader(f)
+            next(read)
+            for row in read:
+                name, phone = row
+                cur.execute(sql, (name, phone))
+        conn.commit()
+    print(f"Imported contacts: {csv_file}")
+
+# ----SELECT----
+
+def select_all():
+    sql = "SELECT * FROM contacts"
+    with conn.cursor() as cur:
+        cur.execute(sql)
+        return cur.fetchall()   
+
+def search(pattern):
+    sql = "SELECT * FROM contacts WHERE name ILIKE %s or phone ILIKE %s"
+    like_p = f"%{pattern}%"
+    with conn.cursor() as cur:
+        cur.execute(sql, (like_p, like_p))
+        return cur.fetchall()
+    
+# ----UPDATE----
+
+def upd_phone(name, nw_phone):
+    sql = "UPDATE contacts SET phone = %s WHERE name = %s"
+    with conn.cursor() as cur:
+        cur.execute(sql, (nw_phone, name)) 
+        conn.commit()
+        print(f"Updated {cur.rowcount} row(s)")
+
+def upd_name(phone, nw_name):
+    sql = "UPDATE contacts SET name = %s WHERE phone = %s"
+    with conn.cursor() as cur:
+        cur.execute(sql, (nw_name, phone))
+        conn.commit()
+        print(f"Updated {cur.rowcount} row(s)")
+        
+# ----DELETE----
+
+def d_name(name):
+    sql = "DELETE FROM contacts WHERE name = %s"
+    with conn.cursor() as cur:
+        cur.execute(sql, (name,))
+        conn.commit()
+        print(f"Deleted {cur.rowcount} row(s)")
+
+def d_phone(phone):
+    sql = "DELETE FROM contacts WHERE phone = %s"
+    with conn.cursor() as cur:
+        cur.execute(sql, (phone,))
+        conn.commit()
+        print(f"Deleted {cur.rowcount} row(s)")
+    
+# ----Main Menu----
+
+def print_contacts(contacts):
+    if not contacts:
+        print("No contacts")
+        return
+    for i in contacts:
+        print(f"[{i[0]}] {i[1]} - {i[2]}")
+        
+def main():
+    table()
+    
+    while True:
+        print("\n---- Phonebook ----")
+        print("1. Show all contacts") 
+        print("2. Add contacts from console") 
+        print("3. Import from CSV")
+        print("4. Search")
+        print("5. Update phone by name")
+        print("6. Update name by phone")
+        print("7. Deleted by name") 
+        print("8. Deleted by phone")
+        print("0. Exit")
+        
+        choice = input("\nChoice: ")
+        if choice == "1":
+            print_contacts(select_all())
+        elif choice == "2":
+            insert_console()
+        elif choice == "3":
+            filename = input("CSV file: ")
+            insert_csv(filename)
+        elif choice == "4":
+            pattern = input("Search: ")
+            print_contacts(search(pattern))
+        elif choice == "5":
+            name = input("Name: ")
+            nw_p = input("New phone: ")
+            upd_phone(name, nw_p)
+        elif choice == "6":
+            phone = input("Phone: ")
+            nw_n = input("New name: ")
+            upd_name(phone, nw_n)
+        elif choice == "7":
+            dname = input("Name: ")
+            d_name(dname)
+        elif choice == "8":
+            dphone = input("Phone: ")
+            d_phone(dphone)
+        elif choice == "0":
+            break
+        else:
+            print("Инвалид другое число выбери")
+    conn.close()
+    print("Goodbye!")
+    
+if __name__ == "__main__":
+    main()        
+         
