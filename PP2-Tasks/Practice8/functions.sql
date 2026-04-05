5a7195147b4a492410b157e75d5a56fbb96ea34e
DarynZhaksylyk <da_zhaksylyk@kbtu.kz>
2026-04-05 17:35:29 +0500

    Prac08

---
+++ PP2-Tasks/Practice8/functions.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION s_contatcs(p TEXT)
+RETURNS TABLE(name VARCHAR, phone VARCHAR) as $$
+BEGIN
+    RETURN QUERY SELECT c.name, c.phone FROM contacts c
+    WHERE c.name ILIKE '%' || p || '%'
+       OR c.phone ILIKE '%' || p || '%';
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION paginated(limitc INT, offsetc INT)
+RETURNS TABLE(name VARCHAR, phone VARCHAR) AS $$
+BEGIN
+    RETURN QUERY SELECT c.name, c.phone FROM contacts c
+    ORDER BY c.name
+    LIMIT limitc OFFSET offsetc;
+END;
+$$ LANGUAGE plpgsql; 
