2774ab7f8a17a8987bdb079e1b5e48f87abb6f5c
DarynZhaksylyk <da_zhaksylyk@kbtu.kz>
2026-04-11 07:53:17 +0500

    Prac08

--- PP2-Tasks/Practice8/functions.sql
+++ PP2-Tasks/Practice8/functions.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION s_contatcs(p TEXT)
+CREATE OR REPLACE FUNCTION s_contacts(p TEXT)
 RETURNS TABLE(name VARCHAR, phone VARCHAR) as $$
 BEGIN
     RETURN QUERY SELECT c.name, c.phone FROM contacts c
